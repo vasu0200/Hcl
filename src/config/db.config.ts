355222bfe6541f8c666099c5df7ba710dae5ad9b
vasu0200 <gollangivasudev@gmail.com>
2024-08-20 21:53:08 +0530

    latest config file

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,10 +2,10 @@ import mysql, { RowDataPacket } from 'mysql2/promise';
 
 // Create a connection pool
 export const pool = mysql.createPool({
-	host: 'localhost', // Database host
-	user: 'root', // Database user
-	password: 'Vasu@2002', // Database password
-	database: 'hclogistics', // Your database name
+	host: 'hclogistics-dev.c3qg4ws80c73.ap-south-1.rds.amazonaws.com', // Database host
+	user: 'user1', // Database user
+	password: 'password', // Database password
+	database: 'hclogistics_dev', // Your database name
 	waitForConnections: true, // Wait for connections to be available
 	connectionLimit: 100, // Max number of connections in the pool
 	queueLimit: 0, // Max number of connection requests to queue
